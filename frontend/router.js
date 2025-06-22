dc6dc1bbe626660639984713ecae860c96c46e94
FerSV4 <ferandressc@hotmail.com>
2025-06-21 22:03:37 -0400

    Solucion conflicto con Git

--- frontend/router.js
+++ frontend/router.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const navigateTo = path => {
     window.location.hash = path;
 };
@@ -53,3 +54,33 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+=======
+
+import './components/front-page.js';
+import './components/contact-page.js';
+
+
+
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => document.createElement('front-page') },
+        { path: "/contact", view: () => document.createElement('contact-page') },
+    ];
+
+    const path = location.hash.slice(1).toLowerCase() || "/";
+    let match = routes.find(route => route.path === path);
+
+    if (!match) {
+        match = routes[0];
+        window.location.hash = "";
+    }
+
+    const app = document.querySelector("#app-root");
+    if (!app) return;
+    app.innerHTML = ""; 
+    app.appendChild(match.view()); 
+};
+
+window.addEventListener("hashchange", router);
+window.addEventListener("load", router);
+>>>>>>> 0602720 (Resolucion de conflicto GIT)
