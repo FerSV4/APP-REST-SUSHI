e7599477f9f88c5b438a17fcd9f09b36072067cc
FerSV4 <ferandressc@hotmail.com>
2025-06-30 08:48:29 -0400

    Update reservaController.js
    Formateo de fecha

--- backend/controllers/reservaController.js
+++ backend/controllers/reservaController.js
@@ -2,16 +2,20 @@ const prisma = require('../db');
 
 const crearReserva = async (req, res) => {
     const { name, phone, email, guests, date, time } = req.body;
+    
     const usuarioId = req.usuario ? req.usuario.id : null;
-    const fechaReserva = new Date(`${date}T${time}:00`);
+
+    const fechaParaDb = new Date(date);
+    const horaParaDb = new Date(`1970-01-01T${time}:00`);
 
     const nuevaReserva = await prisma.reservas.create({
         data: {
             usuario_id: usuarioId,
             nombre_cliente: name,
             contacto_cliente: `${email} / ${phone}`,
-            fecha: fechaReserva,
-            hora: fechaReserva,
+            fecha: fechaParaDb,
+            hora: horaParaDb,
+
             numero_comensales: parseInt(guests),
             estado: 'solicitada',
         }
